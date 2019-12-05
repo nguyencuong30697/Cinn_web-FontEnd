db8db6f47fa22aee2556b6c60716ed5bea07aca4
Nguyen Cuong <nguyencuong.3061997@gmail.com>
2019-12-06 01:59:28 +0700

    Update File

---
+++ README.md
@@ -0,0 +1,1 @@
+Cinn_web-FontEnd
---
+++ cinn-web-fontend/src/pages/LogginScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class LoginScreen extends React.Component{
+    render(){
+        return(
+            <div>Login Screen</div>
+        );
+    }
+}
+
+export default LoginScreen;
